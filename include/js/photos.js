e232818cfe066c941416cbc56063667f09f9aa3c
Romain Caneill-Lortal <rcane@misu197.misu.su.se>
2016-05-26 18:28:43 +0200

    testPics

--- include/js/photos.js
+++ include/js/photos.js
@@ -5,15 +5,23 @@ function showBig(id){
     var state_new = state;
     if (!state || old_id !=  id){
         var showBigItem = document.querySelector(id);
-        showBigItem.classList.remove("col-xs-6 col-sm-4 col-md-4");
-        showBigItem.classList.add("col-xs-12 col-sm-12 col-md-12");
+        showBigItem.classList.remove("col-xs-6");
+        showBigItem.classList.remove("col-sm-4");
+        showBigItem.classList.remove("col-md-4");
+        showBigItem.classList.add("col-xs-12");
+        showBigItem.classList.add("col-sm-12");
+        showBigItem.classList.add("col-md-12");
         alert('test');
         state_new = true;
     }
     if (((old_id==id && state) || old_id !=  id) && old_id != '') {
         var showNormalItem = document.querySelector(old_id);
-        showNormalItem.classList.remove("col-xs-12 col-sm-12 col-md-12");
-        showNormalItem.classList.add("col-xs-6 col-sm-4 col-md-4");
+        showNormalItem.classList.remove("col-xs-12");
+        showNormalItem.classList.remove("col-sm-12");
+        showNormalItem.classList.remove("col-md-12");
+        showNormalItem.classList.add("col-xs-6");
+        showNormalItem.classList.add("col-sm-4");
+        showNormalItem.classList.add("col-md-4");
         alert('test');
         state_new = (old_id !=  id);
     }
