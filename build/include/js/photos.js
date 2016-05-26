707d92952553cec52c00d37b08fe8d94d75f5f9b
Romain Caneill-Lortal <rcane@misu197.misu.su.se>
2016-05-26 18:24:26 +0200

    testPics

--- build/include/js/photos.js
+++ build/include/js/photos.js
@@ -7,12 +7,14 @@ function showBig(id){
         var showBigItem = document.querySelector(id);
         showBigItem.classList.remove("col-xs-6 col-sm-4 col-md-4");
         showBigItem.classList.add("col-xs-12 col-sm-12 col-md-12");
+        alert('test');
         state_new = true;
     }
     if (((old_id==id && state) || old_id !=  id) && old_id != '') {
         var showNormalItem = document.querySelector(old_id);
         showNormalItem.classList.remove("col-xs-12 col-sm-12 col-md-12");
         showNormalItem.classList.add("col-xs-6 col-sm-4 col-md-4");
+        alert('test');
         state_new = (old_id !=  id);
     }
     state = state_new;
