3fae7abb6e692096ecd42c6f854639bf41081d5a
Romain Caneill-Lortal <rcane@misu197.misu.su.se>
2016-05-27 11:57:30 +0200

    testPics

--- include/js/photos.js
+++ include/js/photos.js
@@ -5,22 +5,26 @@ function showBig(id){
     var state_new = state;
     if (!state || old_id !=  id){
         var showBigItem = document.querySelector(id);
+        alert('big 0 '+id)
         showBigItem.classList.remove("col-xs-6");
         showBigItem.classList.remove("col-sm-4");
         showBigItem.classList.remove("col-md-4");
         showBigItem.classList.add("col-xs-12");
         showBigItem.classList.add("col-sm-12");
         showBigItem.classList.add("col-md-12");
+        alert('big 1 '+id)
         state_new = true;
     }
     if (((old_id==id && state) || old_id !=  id) && old_id != '') {
         var showNormalItem = document.querySelector(old_id);
+        alert('normal 0 '+id)
         showNormalItem.classList.remove("col-xs-12");
         showNormalItem.classList.remove("col-sm-12");
         showNormalItem.classList.remove("col-md-12");
         showNormalItem.classList.add("col-xs-6");
         showNormalItem.classList.add("col-sm-4");
         showNormalItem.classList.add("col-md-4");
+        alert('normal 1 '+id)
         state_new = (old_id !=  id);
     }
     state = state_new;
