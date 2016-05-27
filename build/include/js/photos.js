3c6f3253a1dd02d66a298cf624c8c5f34bea715a
Romain Caneill-Lortal <rcane@misu197.misu.su.se>
2016-05-27 12:20:46 +0200

    testPics

--- build/include/js/photos.js
+++ build/include/js/photos.js
@@ -2,29 +2,30 @@ var old_id = '';
 var state = false
 
 function showBig(id){
+    alert('42');
     var state_new = state;
     if (!state || old_id !=  id){
         var showBigItem = document.querySelector(id);
-        alert('test');
+        alert('big 0 '+id);
         showBigItem.classList.remove("col-xs-6");
         showBigItem.classList.remove("col-sm-4");
         showBigItem.classList.remove("col-md-4");
         showBigItem.classList.add("col-xs-12");
         showBigItem.classList.add("col-sm-12");
         showBigItem.classList.add("col-md-12");
-        alert('test');
+        alert('big 1 '+id);
         state_new = true;
     }
     if (((old_id==id && state) || old_id !=  id) && old_id != '') {
         var showNormalItem = document.querySelector(old_id);
-        alert('test');
+        alert('normal 0 '+id);
         showNormalItem.classList.remove("col-xs-12");
         showNormalItem.classList.remove("col-sm-12");
         showNormalItem.classList.remove("col-md-12");
         showNormalItem.classList.add("col-xs-6");
         showNormalItem.classList.add("col-sm-4");
         showNormalItem.classList.add("col-md-4");
-        alert('test');
+        alert('normal 1 '+id);
         state_new = (old_id !=  id);
     }
     state = state_new;
@@ -33,4 +34,14 @@ function showBig(id){
 
 function myFunction() {
     document.getElementById("demo").innerHTML = "coucou";
+}
+
+function void(){
+
+        showBigItem.classList.remove("col-xs-6");
+        showBigItem.classList.remove("col-sm-4");
+        showBigItem.classList.remove("col-md-4");
+        showBigItem.classList.add("col-xs-12");
+        showBigItem.classList.add("col-sm-12");
+        showBigItem.classList.add("col-md-12");
 }
--- photos/2016-05-26-test-photos.md
+++ photos/2016-05-26-test-photos.md
@@ -7,23 +7,23 @@ tags: photo
 <section class="row">
 <div class="col-xs-6 col-sm-4 col-md-4 " id="asfgsfgwerf" onclick="showBig('#'+id)">
   <a class="thumbnail">
-    <img src="photos/2016-05-08-WE-uto/5F0A7051-1.jpg" class="img-rounded">
+    <img src="/photos/2016-05-08-WE-uto/5F0A7051-1.jpg" class="img-rounded">
   </a>
 </div>
 <div class="thumbnails col-xs-6 col-sm-4 col-md-4" id="a5F0A7053-1jpg" onclick="showBig('#'+id)">
-  <img src="photos/2016-05-26-test/5F0A7053-1.jpg" class="img-rounded" />
+  <img src="/photos/2016-05-26-test/5F0A7053-1.jpg" class="img-rounded" />
 </div>
 <div class="thumbnails col-xs-6 col-sm-4 col-md-4" id="a5F0A7061-1.jpg" onclick="showBig('#'+id)">
-  <img src="photos/2016-05-26-test/5F0A7061-1.jpg" class="img-rounded" />
+  <img src="/photos/2016-05-26-test/5F0A7061-1.jpg" class="img-rounded" />
 </div>
 <div class="thumbnails col-xs-6 col-sm-4 col-md-4" id="a5F0A7068-1jpg" onclick="showBig('#'+id)">
-  <img src="photos/2016-05-26-test/5F0A7068-1.jpg" class="img-rounded" />
+  <img src="/photos/2016-05-26-test/5F0A7068-1.jpg" class="img-rounded" />
 </div>
 <div class="thumbnails col-xs-6 col-sm-4 col-md-4" id="a5F0A7072-1jpg" onclick="showBig('#'+id)">
-  <img src="photos/2016-05-26-test/5F0A7072-1.jpg" class="img-rounded" />
+  <img src="/photos/2016-05-26-test/5F0A7072-1.jpg" class="img-rounded" />
 </div>
 <div class="thumbnails col-xs-6 col-sm-4 col-md-4" id="a5F0A7075-1jpg" onclick="showBig('#'+id)">
-  <img src="photos/2016-05-26-test/5F0A7075-1.jpg" class="img-rounded" />
+  <img src="/photos/2016-05-26-test/5F0A7075-1.jpg" class="img-rounded" />
 </div>
 </section>
 
